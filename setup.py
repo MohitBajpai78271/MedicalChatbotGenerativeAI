726cf069021c38c62c71dc13501c0cd5418a78c8
MohitBajpai78271 <mohitbajpai997@gmail.com>
2025-03-26 16:07:12 +0530

    2nd

--- setup.py
+++ setup.py
@@ -1,24 +1,9 @@
-# from setuptools import find_packages, setup
-
-# setup (
-#     name = 'GenerativeAI Project',
-#     version = '0.0.0',
-#     author ='Mohit Bajpai',
-#     author_email = 'mohitbajpai998@gmail.com',
-#     install_requires = [
-#         "sentence-transformers==2.2.2",
-#         "langchain",
-#         "flask",
-#         "pypdf",
-#         "python-dotenv",
-#         "pinecone[grpc]",
-#         "langchain-pinecone",
-#         "langchain_community",
-#         "langchain_openai",
-#         "langchain_experimental"
-#     ],
-#     packages = find_packages(),
-# )
-from setuptools import setup
-if __name__ == '__main__':
-    setup()
+from setuptools import find_packages, setup
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Mohit Bajpai',
+    author_email = 'mohitbajpai998@gmail.com',
+    install_requires = [],
+    packages = find_packages(),
+)
